0488721785ff0eeeee499aac179f908bf75c2c3c
Coleman <coleman.r.alex@gmail.com>
2017-01-21 09:24:09 -0500

    Added print output lines

--- DateCalc.java
+++ DateCalc.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 
 public class DateCalc {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Date Calculator");
         System.out.println("Enter All Information in Numerical Format");
@@ -40,36 +40,37 @@ public class DateCalc {
 
         //Calculate Years
         int years = y1 - y2; //Still need to add leap years
-        int leap = years % 4;//Adds days of leap years to final answer
-        int leapDay = y2 %4;//Helps find if month of February is a leap year for the day calculation.
+        int leap = years / 4;//Adds days of leap years to final answer
+        int leapDay = y2 % 4;//Helps find if month of February is a leap year for the day calculation.
 
         //Calculate Months
         int months = (12 - m2) + m1;
 
         //Calculate Days
         // Calculates  if the month is 28, 29, 30, or 31 days
-        if(m2 == 4) {
+        if (m2 == 4) {
             d2 = (30 - d2);
-        }
-        else if (m2 == 6) {
+        } else if (m2 == 6) {
             d2 = (30 - d2);
-        }
-        else if (m2 == 9) {
+        } else if (m2 == 9) {
             d2 = (30 - d2);
-        }
-        else if (m2 == 11) {
+        } else if (m2 == 11) {
             d2 = (30 - d2);
-        }
-        else if (leapDay / 4 == 1 && m2 == 2) {
+        } else if (leapDay == 0 && m2 == 2) {
             d2 = (29 - d2);
-        }
-        else if (leapDay / 4 != 1) {
+        } else if (leapDay / 4 != 1) {
             d2 = (28 - d2);
-        }
-        else {
+        } else {
             d2 = (31 - d2);
         }
-        
-    }
+        //Find Difference of Days
+        int days = d1 - d2;
 
+        //Adding leap days to days
+        days = days + leap;
+
+        System.out.println("The difference between the two dates are: ");
+        System.out.println(years + "years, " + months + "months, and " + days + "days.");
+    }
 }
+
